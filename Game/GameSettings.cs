75f62fd7b0a80965fa91446c2e817ffeeb9f058e
Jamination <jamesycraft@gmail.com>
2020-09-26 15:57:41 +0100

    Did shit.

--- Game/GameSettings.cs
+++ Game/GameSettings.cs
@@ -4,13 +4,13 @@ namespace ShitGame
 {
     public static class GameSettings
     {
-        public const int StartWindowWidth = 1920;
-        public const int StartWindowHeight = 1080;
+        public const int StartWindowWidth = 1280;
+        public const int StartWindowHeight = 720;
         
         public const int VirtualWindowWidth = 640;
         public const int VirtualWindowHeight = 360;
         
-        public const bool StartFullScreen = true;
+        public const bool StartFullScreen = false;
 
         public static readonly Color ClearColour = new Color(0f, .05f, .1f, 1f);
     }
