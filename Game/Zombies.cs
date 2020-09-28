9ffb48c8c0bbc54449730399e1bc9d015f8ce214
Dcrew <dean.reynolds@live.co.uk>
2020-09-28 19:32:23 +0100

    zombies are alive!

--- Game/Zombies.cs
+++ Game/Zombies.cs
@@ -1,24 +1,24 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Xna.Framework;
 using ShitGame.Components;
 using tainicom.Aether.Physics2D.Dynamics;
 
-namespace ShitGame
-{
-    public static class Zombies
-    {
+namespace ShitGame {
+    public static class Zombies {
         public const uint MaxZombies = 500;
-        
+
+        public static float MoveSpeed { get; private set; } = Data.ToSim(20);
+
         public static Sprite[] Sprites = new Sprite[MaxZombies];
         public static Body[] Bodies = new Body[MaxZombies];
         public static bool[] Active = new bool[MaxZombies];
 
         private static Stack<uint> _freeZombies = new Stack<uint>();
 
-        public static void Load()
-        {
+        public static void Load() {
             _freeZombies.Clear();
             for (uint i = 0; i < MaxZombies; i++)
                 _freeZombies.Push(i);
@@ -26,16 +26,29 @@ namespace ShitGame
 
         public static uint GetInactiveZombie() => _freeZombies.Pop();
 
-        public static void Update()
-        {
+        public static void Update() {
+            for (uint i = 0; i < MaxZombies; i++) {
+                if (Active[i]) {
+                    int closeP = -1;
+                    float closePD = float.MaxValue;
+                    for (int j = 0; j < Players.MaxPlayers; j++) {
+                        float d = Vector2.DistanceSquared(Players.Bodies[j].Position, Bodies[i].Position);
+                        if (d < closePD) {
+                            closePD = d;
+                            closeP = j;
+                        }
+                    }
+                    if (closeP != -1 && Data.World.RayCast(Bodies[i].Position, Players.Bodies[closeP].Position).FirstOrDefault() == Players.Bodies[closeP].FixtureList.FirstOrDefault()) {
+                        float a = MathF.Atan2(Players.Bodies[closeP].Position.Y - Bodies[i].Position.Y, Players.Bodies[closeP].Position.X - Bodies[i].Position.X);
+                        Bodies[i].ApplyForce(new Vector2(MathF.Cos(a) * MoveSpeed, MathF.Sin(a) * MoveSpeed));
+                    }
+                }
+            }
         }
 
-        public static void Draw()
-        {
-            for (uint i = 0; i < MaxZombies; i++)
-            {
-                if (Active[i])
-                {
+        public static void Draw() {
+            for (uint i = 0; i < MaxZombies; i++) {
+                if (Active[i]) {
                     Functions.Draw(ref Sprites[i], Bodies[i], Vector2.One * .25f);
                 }
             }
